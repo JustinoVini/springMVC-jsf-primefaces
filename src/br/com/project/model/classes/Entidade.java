94573750e7041ace4041d99e6344399182dc646c
focaG <60098824+Edamatec@users.noreply.github.com>
2023-12-27 14:22:46 -0300

    Feat: Criação dos xmls de configuração do spring boot/security

--- src/br/com/project/model/classes/Entidade.java
+++ src/br/com/project/model/classes/Entidade.java
@@ -19,6 +19,8 @@ public class Entidade implements Serializable {
 	private String ent_login = null;
 
 	private String ent_senha = null;
+	
+	private Boolean ent_inativo = false;
 
 	public String getEnt_login() {
 		return ent_login;
@@ -42,6 +44,14 @@ public class Entidade implements Serializable {
 
 	public Long getEnt_codigo() {
 		return ent_codigo;
+	}
+	
+	public void setEnt_inativo(Boolean ent_inativo) {
+		this.ent_inativo = ent_inativo;
+	}
+	
+	public Boolean getEnt_inativo() {
+		return ent_inativo;
 	}
 
 }

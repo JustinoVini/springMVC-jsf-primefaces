d33d22dfa9caf8756eda71d1385acff5152ff7c8
focaG <60098824+Edamatec@users.noreply.github.com>
2024-03-07 11:53:06 -0300

    Feat: Continuação do campo de pesquisa

--- src/br/com/project/bean/geral/BeanManagedViewAbstract.java
+++ src/br/com/project/bean/geral/BeanManagedViewAbstract.java
@@ -1,8 +1,17 @@
 package br.com.project.bean.geral;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import javax.faces.model.SelectItem;
+
 import org.springframework.stereotype.Component;
 
 import br.com.framework.interfac.crud.InterfaceCrud;
+import br.com.project.annotation.IdentificaCampoPesquisa;
 import br.com.project.report.util.BeanReportView;
 
 @Component
@@ -16,12 +25,53 @@ public abstract class BeanManagedViewAbstract extends BeanReportView {
 
 	public ObjetoCampoConsulta objetoCampoConsultaSelecionado;
 
+	public List<SelectItem> listaCampoPesquisa;
+
 	public ObjetoCampoConsulta getObjetoCampoConsultaSelecionado() {
 		return objetoCampoConsultaSelecionado;
 	}
 
 	public void setObjetoCampoConsultaSelecionado(ObjetoCampoConsulta objetoCampoConsultaSelecionado) {
 		this.objetoCampoConsultaSelecionado = objetoCampoConsultaSelecionado;
+	}
+
+	public List<SelectItem> getListaCampoPesquisa() {
+
+		listaCampoPesquisa = new ArrayList<SelectItem>();
+		List<ObjetoCampoConsulta> listTemp = new ArrayList<ObjetoCampoConsulta>();
+
+		for (Field field : getClassImplement().getDeclaredFields()) {
+			if (field.isAnnotationPresent(IdentificaCampoPesquisa.class)) {
+				String descricao = field.getAnnotation(IdentificaCampoPesquisa.class).descricaoCampo();
+				String descricaoCampoPesquisa = field.getAnnotation(IdentificaCampoPesquisa.class).campoConsulta();
+				int isPrincipal = field.getAnnotation(IdentificaCampoPesquisa.class).principal();
+
+				ObjetoCampoConsulta objetoCampoConsulta = new ObjetoCampoConsulta();
+				objetoCampoConsulta.setDescricao(descricao);
+				objetoCampoConsulta.setCampoBanco(descricaoCampoPesquisa);
+				objetoCampoConsulta.setTipoClass(field.getType().getCanonicalName());
+				objetoCampoConsulta.setPrincipal(isPrincipal);
+				listTemp.add(objetoCampoConsulta);
+			}
+		}
+
+		orderReverse(listTemp);
+		
+		for (ObjetoCampoConsulta objetoCampoConsulta : listTemp) {
+			listaCampoPesquisa.add(new SelectItem(objetoCampoConsulta));
+		}
+
+		return listaCampoPesquisa;
+	}
+
+	private void orderReverse(List<ObjetoCampoConsulta> listTemp) {
+		Collections.sort(listTemp, new Comparator<ObjetoCampoConsulta>() {
+
+			@Override
+			public int compare(ObjetoCampoConsulta objt1, ObjetoCampoConsulta objt2) {
+				return objt1.getPrincipal().compareTo(objt2.getPrincipal());
+			}
+		});
 	}
 
 }
